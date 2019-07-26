2d2a9a6bddd8d0d28ffb4e09dbab995921718089
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-26 23:06:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,30 +1,107 @@
-var cart = [];
 
+var cart = [];
+"use strict";
 function getCart() {
- return cart;
+  return cart;
 }
 
 function setCart(c) {
   cart = c;
-  return cart;
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random() * 100) + 1;
+  var item_object = {itemName: item};
+  item_object['itemPrice'] = price;
+  cart.push(item_object);
+  return (item_object.itemName + " has been added to your cart.");
 }
+
+
+addToCart("banana");
+addToCart("apple");
+console.log(cart);
+
+
 
 function viewCart() {
-  // write your code here
-}
+  const l = cart.length;
+  var items = [];
+  var prices=[];
 
-function total() {
-  // write your code here
-}
+  if (!l) {
+    return ("Your shopping cart is empty.");
+  }
+      else if (l===1){
+      
+           for (var i=0; i < l ; ++i)
+               {items.push(cart[i]['itemName']);
+                prices.push(cart[i]['itemPrice']);
+               }
+           var string = "In your cart, you have ";
+               string+= items + " at $" + prices+".";
+               return string;
+      }
+      else if (l===2){
+
+           for (i = 0; i < l ; ++i)
+               {items.push(cart[i]['itemName']);
+                prices.push(cart[i]['itemPrice']);
+               }
+               string = "In your cart, you have "
+               string+= items[0] + " at $" + prices[0]+", and "+items[1] + " at $" + prices[1]+"."
+               return string
+      }
+            else if (l===3){
+
+           for (i=0; i < l ; ++i)
+               {items.push(cart[i]['itemName']);
+                prices.push(cart[i]['itemPrice']);
+               }
+               string = "In your cart, you have "
+               string+= items[0] + " at $" + prices[0]+", "+items[1] + " at $" + prices[1]+", and "+items[2] + " at $" + prices[2]+"."
+               return string;
+      }
+  }
+
+  
+  
+  
+  
 
 function removeFromCart(item) {
   // write your code here
+  console.log(item);
+  for(let i = 0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item)) {
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  console.log('That item is not in your cart.');
 }
 
+removeFromCart("pear")
+removeFromCart("banana")
+console.log(cart)
+
 function placeOrder(cardNumber) {
-  // write your code here
+  if (!cardNumber) {
+    return console.log("We don't have a credit card on file for you to place your order.");
+  }
+
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+
+  cart = [];
+}
+
+function total() {
+  let t = 0;
+
+  for (var i = 0; i < cart.length; i++) {
+      t += cart[i]['itemPrice'];
+    
+  }
+
+  return t;
 }
