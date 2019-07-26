c9f9f033742d6e27e9c40786d8d8b8a799da8578
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-26 23:06:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -75,10 +75,12 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++) {
     if(cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1);
-      return cart;
+      
     }
   }
   console.log('That item is not in your cart.');
+
+  return cart
 }
 
 removeFromCart("pear")
