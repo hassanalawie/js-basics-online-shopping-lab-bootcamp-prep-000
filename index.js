bacfa0039cf0e28a4383c1c3870831c4bc7c9ded
Hassan Alawie <hassan.alawie10@gmail.com>
2019-07-26 23:31:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -89,9 +89,9 @@ for (var i=0;i<cart.length;i++){
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return console.log("We don't have a credit card on file for you to place your order.");
+    return ("We don't have a credit card on file for you to place your order.");
   }
-
+else
   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
 
   cart = [];
